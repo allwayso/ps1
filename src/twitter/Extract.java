f2e716ab256dc1e396ccc487db2e6979dc95f1d8
allwayso <1784817625@qq.com>
2026-02-20 22:21:17 +0800

    implement two methods and pass the tests

--- src/twitter/Extract.java
+++ src/twitter/Extract.java
@@ -3,8 +3,7 @@
  */
 package twitter;
 
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.time.Instant;
 
 /**
@@ -19,40 +18,71 @@ public class Extract {
     /**
      * Get the time period spanned by tweets.
      * 
-     * @param tweets
-     *            list of tweets with distinct ids, not modified by this method.
-     * @return a minimum-length time interval that contains the timestamp of
-     *         every tweet in the list.
+     * @param tweets list of tweets with distinct ids, not modified by this method.
+     * @return a minimum-length time interval that contains the timestamp of every
+     *         tweet in the list.
      */
     public static Timespan getTimespan(List<Tweet> tweets) {
-        Instant min_stamp=Instant.MAX;
-        Instant max_stamp=Instant.MIN;
-        for(Tweet tweet:tweets) {
-            Instant current_stamp=tweet.getTimestamp();
-            if(current_stamp.isBefore(min_stamp)) min_stamp=current_stamp;
-            if(current_stamp.isAfter(max_stamp)) max_stamp=current_stamp;
+        Instant min_stamp = Instant.MAX;
+        Instant max_stamp = Instant.MIN;
+        for (Tweet tweet : tweets) {
+            Instant current_stamp = tweet.getTimestamp();
+            if (current_stamp.isBefore(min_stamp))
+                min_stamp = current_stamp;
+            if (current_stamp.isAfter(max_stamp))
+                max_stamp = current_stamp;
         }
-        Timespan result=new Timespan(min_stamp,max_stamp);
+        Timespan result = new Timespan(min_stamp, max_stamp);
         return result;
     }
 
     /**
      * Get usernames mentioned in a list of tweets.
      * 
-     * @param tweets
-     *            list of tweets with distinct ids, not modified by this method.
-     * @return the set of usernames who are mentioned in the text of the tweets.
-     *         A username-mention is "@" followed by a Twitter username (as
-     *         defined by Tweet.getAuthor()'s spec).
-     *         The username-mention cannot be immediately preceded or followed by any
-     *         character valid in a Twitter username.
-     *         For this reason, an email address like bitdiddle@mit.edu does NOT 
-     *         contain a mention of the username mit.
-     *         Twitter usernames are case-insensitive, and the returned set may
-     *         include a username at most once.
+     * @param tweets list of tweets with distinct ids, not modified by this method.
+     * @return the set of usernames who are mentioned in the text of the tweets. A
+     *         username-mention is "@" followed by a Twitter username (as defined by
+     *         Tweet.getAuthor()'s spec). The username-mention cannot be immediately
+     *         preceded or followed by any character valid in a Twitter username.
+     *         For this reason, an email address like bitdiddle@mit.edu does NOT
+     *         contain a mention of the username mit. Twitter usernames are
+     *         case-insensitive, and the returned set may include a username at most
+     *         once.
      */
     public static Set<String> getMentionedUsers(List<Tweet> tweets) {
-        throw new RuntimeException("not implemented");
+        Set<String> users = new HashSet<String>();
+        for (Tweet t : tweets) {
+            String text = t.getText();
+            if (!text.contains("@"))
+                continue;
+            int idx = text.indexOf('@');
+            while (idx != -1) {
+                if (idx == 0 || !isValidChar(text.charAt(idx - 1))) {
+                    int end = idx + 1;
+                    while (end < text.length() && isValidChar(text.charAt(end)))
+                        end++;
+                    String name = text.substring(idx + 1, end);
+                    if (end > idx + 1) {
+                        name = name.toLowerCase();
+                        users.add(name);
+                    }
+                }
+                idx = text.indexOf('@', idx + 1);
+            }
+        }
+        return users;
     }
 
+    /*
+     * Judge the legality of character
+     * 
+     * @param c:the character to be judged
+     * 
+     * @return whether the character is among letters (A-Z or a-z), digits,
+     * underscore ("_"), or hyphen ("-")
+     */
+    
+    public static boolean isValidChar(char c) {
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9') || (c == '_') || (c == '-');
+    }
 }
